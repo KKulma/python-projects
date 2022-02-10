8b103057868c9c8b602c68b0765ac6e4d28f65dc
Kasia Kulma <kasia.kulma@effem.com>
2022-02-10 16:37:07 +0000

    finish contact book

--- contact-book.py
+++ contact-book.py
@@ -1,147 +1,131 @@
-class Contact(object):
+import sqlite3
 
-    def __init__(self, name):
-        self.name = name
-        self.surname = None
-        self.email = None
-        self.mobile_phone = None
-        self.address = None
+conn = sqlite3.connect("contacts.db")
+cursor = conn.cursor()
 
-    @property
-    def name(self):
-        return self._name
-
-    @property
-    def surname(self):
-        return self._surname
-
-    @property
-    def email(self):
-        return self._email
-
-    @property
-    def mobile_phone(self):
-        return self._mobile_phone
-
-    @property
-    def address(self):
-        return self._address
+# # initiate the db
+# cursor.execute("CREATE TABLE contacts (id INTEGER PRIMARY KEY, name TEXT, surname TEXT, email TEXT, phone_number TEXT, address TEXT)")
+# data_person_name = [('Michael', 'Fox', "miacheal.fox@email.com", "01234567890", "Mike's address"),
+#                     ('Adam', 'Miller', "adam.miller@email.com", "01234567890", "Adam's address"),
+#                     ('Andrew', 'Peck', "andrew.peck@email.com", "01234567890", "Andy's address"),
+#                     ('James', 'Shroyer', "james.shrover@email.com", "01234567890", "James' address"),
+#                     ('Eric', 'Burger', "eric.burger@email.com", "01234567890", "Eric's address")
+#                     ]
+# cursor.executemany('INSERT INTO contacts(name, surname, email, phone_number, address) VALUES (?, ?, ?, ?, ?)', data_person_name)
+# conn.commit()
 
 
-    @name.setter
-    def name(self, new_name):
-        if new_name is None:
-            self._name = new_name
-        elif isinstance(new_name, str):
-            self._name = new_name
-        else:
-            print("Name must be a character string")
-
-    @surname.setter
-    def surname(self, new_surname):
-        if new_surname is None:
-            self._surname = new_surname
-        elif isinstance(new_surname, str):
-            self._surname = new_surname
-        else:
-            print("Surname must be a character string")
-
-    @email.setter
-    def email(self, new_email):
-        if new_email is None:
-            self._email = new_email
-        elif isinstance(new_email, str):
-            if '@' not in new_email:
-                print("Email must be valid and contain @")
-            else:
-                self._email = new_email
-        else:
-            print("Email must be a character string")
-
-    @mobile_phone.setter
-    def mobile_phone(self, new_mobile_phone):
-        if new_mobile_phone is None:
-            self._mobile_phone = new_mobile_phone
-        elif isinstance(new_mobile_phone, str):
-            self._mobile_phone = new_mobile_phone
-        else:
-            print("Mobile phone must be a character string")
-
-    @address.setter
-    def address(self, new_address):
-        if new_address is None:
-            self._address = new_address
-        elif isinstance(new_address, str):
-            self._address = new_address
-        else:
-            print("Address must be a character string")
-
-    @name.deleter
-    def name(self):
-        del self._name
-
-    @surname.deleter
-    def surname(self):
-        del self._surname
-
-    @email.deleter
-    def email(self):
-        del self._email
-
-    @mobile_phone.deleter
-    def mobile_phone(self):
-        del self._mobile_phone
-
-    @address.deleter
-    def address(self):
-        del self._address
-
-
-# TODO initiate a SQL db
 while True:
-    print("What do you want to do? \n A-add new contact; M-modify the existing contact, Q-quit")
+    print("What do you want to do? \n A-add new contact; M-modify the existing contact, D-delete an entry, S-show all entries,  Q-quit")
     ans = input("").lower()
 
-    if ans == 'a':
+    if ans == 's':
+        rows = cursor.execute("SELECT *  FROM contacts").fetchall()
+        [print(row) for row in rows]
+    elif ans == 'a':
         print("What's your contact's name?")
         name = input("")
-        contact = Contact(name)
+
         print("What's your contact's surname?")
         surname = input("")
-        contact.surname = surname
+
         print("What's your contact's email address?")
         email = input("")
-        contact.email = email
+
         print("What's your contact's mobile phone number?")
         phone = input("")
-        contact.mobile_phone = phone
+
         print("What's your contact's address?")
         address = input("")
-        contact.address = address
 
-        # TODO save the contact to the DB
+        # save the answers in db
+        new_entry = [(name, surname, email, phone, address)]
+        cursor.executemany("INSERT INTO contacts(name, surname, email, phone_number, address) VALUES (?, ?, ?, ?, ?)", new_entry)
+        conn.commit()
 
+        # next steps
         print('Your new contact has been added! Do you want to review all the entries? \n Y/N')
         review = input("").lower()
         if review == 'y':
-            print(f"New contact's name: {contact.name}")
-            print(f"New contact's surname: {contact.surname}")
-            print(f"New contact's email: {contact.email}")
-            print(f"New contact's mobile phone: {contact.mobile_phone}")
-            print(f"New contact's address: {contact.address}")
+            print(f"New contact's name: {name}")
+            print(f"New contact's surname: {surname}")
+            print(f"New contact's email: {email}")
+            print(f"New contact's mobile phone: {phone}")
+            print(f"New contact's address: {address}")
             print("-----------------------")
             continue
         else:
             print("Thanks, that's all!")
             break
 
-    elif ans == 'm':
-        # print("Which contact would you like to modify? Type row number)
-        # TODO retrieve all the entries from the SQL db
-        print("TBC")
+    elif ans in ['m', 'd']:
+        print("Which contact would you like to modify? Type ID")
+        rows = cursor.execute("SELECT * FROM contacts").fetchall()
+        [print(row) for row in rows]
 
+        select_id = input("")
+        selected_list = cursor.execute(
+            "SELECT * FROM contacts WHERE id = ?",
+            (select_id,)).fetchall()
+
+        selected_tuple = selected_list[0]
+        num_rows = len(selected_list)
+
+        if num_rows < 1:
+            print("There's no such name, try again")
+            continue
+        else:
+            if ans == 'd':
+                print(f"You're going to delete the following entry: \n {selected_tuple}")
+                cursor.execute(
+                    "DELETE FROM contacts WHERE id = ?",
+                    (select_id))
+                conn.commit()
+                print("Entry deleted")
+
+            else:
+                print(f"You're going to modify the following entry: \n {selected_tuple}")
+                print("Now, which entry would you like to modify? \n N-name; S-surname; E-email; P-phone number; A-adrress; Q-finished")
+                choice = input("").upper()
+
+                if choice == 'Q':
+                    print("All finished, thanks!")
+                    break
+                elif choice in ["N", "S", "E", "P", "A"]:
+                    print("What's the new value")
+                    entry = input("")
+
+                    lookup_dict = {"N": "name", "S": "surname", "E": "email", "P": "phone_number", "A": "address"}
+                    mod_dict = {"id": select_id, "name": selected_tuple[1], "surname": selected_tuple[2], "email": selected_tuple[3], "phone_number": selected_tuple[4], "address": selected_tuple[5]}
+
+                    mod_dict[lookup_dict[choice]] = entry
+                    sql = ''' UPDATE contacts SET 
+                                    name =:name,
+                                    surname =:surname,
+                                    email =:email,
+                                    phone_number =:phone_number,
+                                    address =:address
+                                    WHERE id =:id'''
+                    cursor = conn.execute(sql, mod_dict)
+                    conn.commit()
+
+                    selected_list = cursor.execute(
+                        "SELECT name, surname, email, phone_number, address FROM contacts WHERE id = ?",
+                        (select_id, )).fetchall()
+                    selected_tuple = selected_list[0]
+                    print("Entry updated!")
+                    print(selected_tuple)
+                    continue
+                else:
+                    print("Your answer doesn't contain either of N, S, E, P, A or Q. Try again")
+                    continue
     elif ans == 'q':
+        if conn:
+            conn.close()
         break
 
     else:
         print("Wrong answer! \n Choose between A-add new contact; M-modify the existing contact, Q-quit")
+
+
+
